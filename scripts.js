e4391c9ce3125d2237a0fa4f112b4b86115e1d8d
Ricardo Schroeder <r.schroeder@vanlanschotkempen.com>
2021-08-20 15:59:05 +0200

    Another try

--- scripts.js
+++ scripts.js
@@ -1,84 +1,30 @@
-treeData = [{
-    "name": "Niclas Superlongsurname",
-    "class": "man",
-    "textClass": "emphasis",
-    "marriages": [{
-      "spouse": {
-        "name": "Iliana",
-        "class": "woman",
-        "extra": {
-            "nickname": "Illi"
-        }
-      },
-      "children": [{
-        "name": "James",
-        "class": "man",
-        "marriages": [{
-          "spouse": {
-            "name": "Alexandra",
-            "class": "woman"
-          },
-          "children": [{
-            "name": "Eric",
-            "class": "man",
-            "marriages": [{
-              "spouse": {
-                "name": "Eva",
-                "class": "woman"
-              }
-            }]
-          }, {
-            "name": "Jane",
-            "class": "woman"
-          }, {
-            "name": "Jasper",
-            "class": "man"
-          }, {
-            "name": "Emma",
-            "class": "woman"
-          }, {
-            "name": "Julia",
-            "class": "woman"
-          }, {
-            "name": "Jessica",
-            "class": "woman"
-          }]
-        }]
-      }]
-    }]
-  }]
-  
-  
-  
-  dTree.init(treeData, {
-    target: "#graph",
-    debug: true,
-    height: 800,
-    width: 1200,
-    callbacks: {
-      nodeClick: function(name, extra) {
-        console.log(name);
-      },
-      textRenderer: function(name, extra, textClass) {
-          // THis callback is optinal but can be used to customize
-        // how the text is rendered without having to rewrite the entire node
-        // from screatch.
-          if (extra && extra.nickname)
-            name = name + " (" + extra.nickname + ")";
-          return "<p align='center' class='" + textClass + "'>" + name + "</p>";
-      },
-      nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-        // This callback is optional but can be used to customize the
-        // node element using HTML.
-        let node = '';
-        node += '<div ';
-        node += 'style="height:100%;width:100%;" ';
-        node += 'class="' + nodeClass + '" ';
-        node += 'id="node' + id + '">\n';
-        node += textRenderer(name, extra, textClass);
-        node += '</div>';
-        return node;
-    }
-    }
+treeJson = d3.json("data.json", function(error, treeData) {
+    dTree.init(treeData,
+              {
+                  target: "#graph",
+                  debug: true,
+                  hideMarriageNodes: true,
+                  marriageNodeSize: 5,
+                  height: 800,
+                  width: 1200,
+                  callbacks: {
+                      nodeClick: function(name, extra) {
+                          alert('Click: ' + name);
+                      },
+                      nodeRightClick: function(name, extra) {
+                          alert('Right-click: ' + name);
+                      },
+                      textRenderer: function(name, extra, textClass) {
+                          if (extra && extra.nickname)
+                              name = name + " (" + extra.nickname + ")";
+                          return "<p align='center' class='" + textClass + "'>" + name + "</p>";
+                      },
+                      marriageClick: function(extra, id) {
+                          alert('Clicked marriage node' + id);
+                      },
+                      marriageRightClick: function(extra, id) {
+                          alert('Right-clicked marriage node' + id);
+                      },
+                  }
+              });
   });
-  
